359fd7e6234613b209f5c9120c1c8561331c91fa
DazedFury <jmtexas19@gmail.com>
2018-09-13 16:55:15 -0500

    feat: Add user input for decimals

---
+++ cmpsc351/labs/lab-3/L3_P1_jcm5814.asm
@@ -0,0 +1,34 @@
+#Julian Marin
+#Lab 3
+#9/13/2018
+
+.data
+	dec1:		.double 0.0
+	dec2:		.double 0.0	
+	promptString1:	.asciiz "Enter decimal 1:\n"
+	promptString2:	.asciiz "Enter decimal 2:\n"
+
+.text
+	#PRINT PROMPT 1
+	li	$v0, 4			#Load syscall print string
+	la	$a0, promptString1	#Set $a0 to promptString
+	syscall				#Execute syscall
+	
+	#INPUT1 INTO dec1
+	li	$v0, 6			#Load syscall read float
+	syscall				#Execute
+	swc1 	$f0, dec1		#Copy input into dec1
+	
+	#PRINT PROMPT 2
+	li	$v0, 4			#Load syscall print string
+	la	$a0, promptString2	#Set $a0 to promptString
+	syscall				#Execute syscall
+	
+	#INPUT1 INTO dec2
+	li	$v0, 6			#Load syscall read float
+	syscall				#Execute
+	swc1 	$f0, dec2		#Copy input into dec2
+	
+	
+	
+	
