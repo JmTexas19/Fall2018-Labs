f3d3f0b8a94b870824e42b2ecc5057e0903f030c
DazedFury <jmtexas19@gmail.com>
2018-09-14 08:36:26 -0500

    feat: Add multiplication and fix syscalls

--- cmpsc351/labs/lab-3/L3_P1_jcm5814.asm
+++ cmpsc351/labs/lab-3/L3_P1_jcm5814.asm
@@ -5,6 +5,7 @@
 .data
 	dec1:		.word 0
 	dec2:		.word 0	
+	result		.word 0
 	promptString1:	.asciiz "Enter decimal 1:\n"
 	promptString2:	.asciiz "Enter decimal 2:\n"
 
@@ -15,9 +16,9 @@
 	syscall				#Execute syscall
 	
 	#INPUT1 INTO dec1
-	li	$v0, 6			#Load syscall read float
+	li	$v0, 5			#Load syscall read decimal
 	syscall				#Execute
-	sw 	$t0, dec1		#Copy input into dec1
+	sw 	$v0, dec1		#Copy input into dec1
 	
 	#PRINT PROMPT 2
 	li	$v0, 4			#Load syscall print string
@@ -25,10 +26,26 @@
 	syscall				#Execute syscall
 	
 	#INPUT1 INTO dec2
-	li	$v0, 6			#Load syscall read float
+	li	$v0, 5			#Load syscall read decimal
 	syscall				#Execute
-	sw	$t0, dec2		#Copy input into dec2
+	sw	$v0, dec2		#Copy input into dec2
 	
+	#PUT INTO REGISTERS
+	lw	$t0, dec1
+	lw	$t1, dec2
+
+#CHECK BIT
+loop:
+	andi	$t2, $t1, 1		#Check if bit is clear; #t2 bit_check
+	beqz	$t2, clearBit		#Branch if bit is clear
+	addu	$t3, $t3, $t0		#Add dec2 to result if bit is not clear; #t3 result
+
+#MULTIPLY AND SHIFT
+clearBit:
+	sll	$t0, $t0, 1		#Shift dec1 left one bit to multiply by power of 2
+	srl	$t1, $t1, 1		#Shift dec2 right one bit to check next bit
+	bnez	$t1, loop		#If dec2 is not equal to zero, loop again, otherwise done
+	sw	$t3, result		#Store result into label
 	
-	
+
 	
