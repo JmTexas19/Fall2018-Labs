001d95cdd84e6bd1427c86fdf788478ef41d3028
Julian Caine Marin <jcm5814@win.psu.edu>
2018-09-18 12:39:15 -0400

    feat: Add setup and questions

--- cmpsc497/lab-9/lab-9.py
+++ cmpsc497/lab-9/lab-9.py
@@ -5,3 +5,104 @@ Julian Marin
 Lab 9
 """
 
+
+# Load libraries
+import pandas
+from pandas.tools.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import cross_validation
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+
+# shape
+print(dataset.shape)
+
+# head
+print(dataset.head(20))
+
+# descriptions
+print(dataset.describe())
+
+# class distribution
+print(dataset.groupby('class').size())
+
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+plt.show()
+
+##Explain the plots. This should give you a much clearer idea of the distribution of the input attributes (What is Box and Whisker Plots?).
+
+# histograms
+dataset.hist()
+plt.show()
+
+##What is historgram?
+
+# scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
+
+##What is scatterplot?
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = cross_validation.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+# Test options and evaluation metric
+num_folds = 10
+num_instances = len(X_train)
+seed = 7
+scoring = 'accuracy'
+
+# Spot Check Algorithms
+models = []
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('NB', GaussianNB()))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = cross_validation.KFold(n=num_instances, n_folds=num_folds, random_state=seed)
+    cv_results = cross_validation.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+##Which algorithm has more estimated accuracy score?
+    
+# Compare Algorithms
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+
+##Which samples achieving 100% accuracy?
+
+# Make predictions on validation dataset
+knn = KNeighborsClassifier()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+
+##What is the percentage of the accuracy?
